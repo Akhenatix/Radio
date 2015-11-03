f5701b64a70506903ce06bbfc748a8da3e1b3124
Akhenatix <al.caillaud@laposte.net>
2015-11-03 13:48:26 +0100

    requête avec serveur web

--- test.py
+++ test.py
@@ -1,13 +1,48 @@
 import urllib2
+# from urllib2 import request
+# import urllib.request
 import pygame
+import os
 
-mp3file = urllib2.urlopen("http://192.168.137.1:8125")
-output = open('test.mp3','wb')
+
+longitude = -20;
+latitude = 20;
+
+# Desactiver le pare feu
+print str("reading from server ") + str(longitude) +" "+ str(latitude);
+requestURL = "http://192.168.137.1:8125/?longitude="+str(longitude)+"&latitude="+str(latitude);
+
+
+
+print "http://192.168.137.1:8125/?longitude="+str(longitude)+"&latitude="+str(latitude);
+
+mp3file = urllib2.urlopen("http://192.168.137.1:8125/?longitude="+str(longitude)+"&latitude="+str(latitude))
+
+
+print "file "
+output = open('musique.mp3','wb')
 output.write(mp3file.read())
 output.close()
+print "end"
+
+# from subprocess import call
+# print call(["wget", "-O", "musique.mp3", requestURL])
+
+
+
+# urllib.request.urlretrieve(requestURL, 'musique.mp3')
+
+
+
+print os.path.dirname(os.path.realpath(__file__))
 
 pygame.mixer.init()
-pygame.mixer.music.load("test.mp3")
+pygame.mixer.music.load("musique.mp3")
 pygame.mixer.music.play()
+
 while pygame.mixer.music.get_busy() == True:
     continue
+
+os.remove('musique.mp3');
+
+
